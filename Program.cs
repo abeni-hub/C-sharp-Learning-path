969ab5740730c93bed17a2b5cc02e9719bf36c18
abeni <abeni@email.com>
2025-09-01 17:43:30 -0700

    second commit

--- Program.cs
+++ Program.cs
@@ -1,3 +1,20 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
-Console.WriteLine("I am just started learning C#");
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        // Numbers
+        int age = 25;
+        double price = 99.99;
+
+        // Text
+        string name = "Adawu";
+
+        // True/False
+        bool isStudent = true;
+
+        // Print them
+        Console.WriteLine($"Name: {name}, Age: {age}, Price: {price}, Student: {isStudent}");
+    }
+}
